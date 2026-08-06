2e2eaf148776d5b9839cdd92c64f758e61fdea2e
56738 <git@56738.me>
2026-08-06 08:26:53 +0200

    Only start interpolation if the render state changed
    Otherwise, the client will restart the previous interpolation

--- gizmo-bukkit/src/main/java/me/m56738/gizmo/bukkit/display/v1_20_2/DisplayCubeGizmo_v1_20_2.java
+++ gizmo-bukkit/src/main/java/me/m56738/gizmo/bukkit/display/v1_20_2/DisplayCubeGizmo_v1_20_2.java
@@ -4,16 +4,24 @@ import me.m56738.gizmo.bukkit.display.JOMLMapper;
 import me.m56738.gizmo.bukkit.display.v1_19_4.DisplayCubeGizmo_v1_19_4;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Display;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Transformation;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
 @ApiStatus.Internal
 public class DisplayCubeGizmo_v1_20_2 extends DisplayCubeGizmo_v1_19_4 {
+    private @Nullable BlockData lastBlock;
+    private @Nullable Transformation lastTransformation;
+    private @Nullable Display.Billboard lastBillboard;
+
     public DisplayCubeGizmo_v1_20_2(Player player, Plugin plugin, JOMLMapper mapper) {
         super(player, plugin, mapper);
     }
@@ -44,6 +52,18 @@ public class DisplayCubeGizmo_v1_20_2 extends DisplayCubeGizmo_v1_19_4 {
     @Override
     protected void update(@NotNull BlockDisplay entity) {
         super.update(entity);
-        entity.setInterpolationDelay(0);
+
+        BlockData block = entity.getBlock();
+        Transformation transformation = entity.getTransformation();
+        Display.Billboard billboard = entity.getBillboard();
+
+        if (!block.equals(lastBlock)
+                || !transformation.equals(lastTransformation)
+                || !billboard.equals(lastBillboard)) {
+            entity.setInterpolationDelay(0);
+            lastBlock = block;
+            lastTransformation = transformation;
+            lastBillboard = billboard;
+        }
     }
 }
